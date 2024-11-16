9da2784cf8950e9e06820f2439656129727dbf0b
Teodora Nikolić <154604837+teodora525@users.noreply.github.com>
2024-11-16 15:46:00 +0100

    Updates on homepage
    Add .css for homepage

--- event-planner/src/app/app.module.ts
+++ event-planner/src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { ProductsComponent } from './products/products.component';
+import {AllEventsComponent} from './all-events/all-events.component';
+import {AppRoutingModule} from './app.routes';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
     ProductsComponent,
+    HomeComponent,
+    AllEventsComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
