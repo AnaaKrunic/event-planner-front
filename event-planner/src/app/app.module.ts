b04d4d09520f14f24160764647a9028844fdc22c
teodora525 <154604837+teodora525@users.noreply.github.com>
2024-11-18 19:11:36 +0100

    Updated module and routing

--- event-planner/src/app/app.module.ts
+++ event-planner/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { ProductsComponent } from './products/products.component';
-import {AllEventsComponent} from './all-events/all-events.component';
-import {AppRoutingModule} from './app.routes';
+import { AllEventsComponent } from './all-events/all-events.component';
+import { AllProductsAndServicesComponent } from './all-products-and-services/all-products-and-services.component'; // Import komponenta
+import { AppRoutingModule } from './app.routes';
 import { HomeComponent } from './home/home.component';
-import {FormsModule} from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import {FormsModule} from '@angular/forms';
     EventsComponent,
     ProductsComponent,
     HomeComponent,
-    AllEventsComponent
+    AllEventsComponent,
+    AllProductsAndServicesComponent
   ],
   imports: [
     BrowserModule,
@@ -25,4 +26,4 @@ import {FormsModule} from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
